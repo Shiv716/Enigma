94178fdc41a2bf4e80eadaa3b7c5a306350a9058
shivangchaudhary <cshivang2002@gmail.com>
2022-03-11 23:57:44 +0530

    GUI of Enigma

--- src/EnigmaFolder/EnigmaController.java
+++ src/EnigmaFolder/EnigmaController.java
@@ -1,15 +1,16 @@
 package EnigmaFolder;
 
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.awt.event.ActionEvent;
 
 public class EnigmaController {
 
-    public TextField Rotor1Display;
-    public TextField Rotor2Display;
-    public TextField Rotor3Display;
-    public TextField  Ciphertext;
+    public Label Rotor1Display;
+    public Label Rotor2Display;
+    public Label Rotor3Display;
+    public Label  Ciphertext;
     public TextField EnterText;
     public Slider Rotor1;
     public Slider Rotor2;
@@ -17,7 +18,7 @@ public class EnigmaController {
 
 
     //Storing the letter provided by the user:-
-    private static String d1;
+    public static String d1;
 
     public static void setD1(String d1) {
         EnigmaController.d1 = d1;
@@ -25,28 +26,19 @@ public class EnigmaController {
 
     //Inputs letter from the user:-
     public void cipherInput(ActionEvent actionEvent){
-        EnterText.getText();
-        d1 = new String(EnterText.getText());
+        try{
+            d1 = EnterText.getText();
+        }
+        catch(Exception e){
+            System.out.println("Please enter Capital Letters only");
+        }
+
         EnterText.setText(""+d1);
     }
 
 
     //Setting values at Rotor1 Slider:-(First need to check if will work nicely.)
-    public void rotor1(ActionEvent actionEvent){
-        //Assigning the slider:-
-        Rotor1 = new Slider();
-        //Min value
-        Rotor1.setMin(1);
-        //Max value
-        Rotor1.setMax(26);
-        //Setting initial value
-        Rotor1.setValue(1);
-        //To display ticks
-        Rotor1.setShowTickMarks(true);
-        //Value between two ticks
-        Rotor1.setMajorTickUnit(1);
-        //Space between two ticks
-        Rotor1.setBlockIncrement(2);
+   {
         
         // Using the switch statement to assign letters to digits chosen:-
         //MAYBE USE VALUE TO SEE WHAT IS CHOSEN IN SLIDER?
@@ -60,49 +52,41 @@ public class EnigmaController {
 //        }
     }
 
-
-
-
     //Setting values at Rotor2 Slider:-(First need to check if will work nicely.)
-    public void rotor2(ActionEvent actionEvent){
-        Rotor2 = new Slider();
-        Rotor2.setMin(0);
-        Rotor2.setMax(100);
-        Rotor2.setValue(40);
-        Rotor2.setShowTickLabels(true);
-        Rotor2.setShowTickMarks(true);
-        Rotor2.setMajorTickUnit(50);
-        Rotor2.setMinorTickCount(5);
-        Rotor2.setBlockIncrement(10);
-    }
-
-
 
     //Setting values at Rotor3 Slider:-(First need to check if will work nicely.)
-    public void rotor3(ActionEvent actionEvent){
-        Rotor3 = new Slider();
-        Rotor3.setMin(0);
-        Rotor3.setMax(26);
-        Rotor3.setValue(1);
-        Rotor3.setShowTickLabels(true);
-        Rotor3.setShowTickMarks(true);
-        Rotor3.setMajorTickUnit(13);
-        Rotor3.setMinorTickCount(5);
-        Rotor3.setBlockIncrement(5);
-    }
 
     //Outputs ciphertext:-
-    public void cipherOutput(ActionEvent actionEvent){
-//        EnterText.getText();
-//        String d1 = new String(EnterText.getText());
-        Ciphertext.setText(""+d1);
+
+    public void cipherInput(javafx.event.ActionEvent actionEvent) {
+        EnterText.getText();
+        String d1 = new String(EnterText.getText());
+        Ciphertext.setText(""+d1.toUpperCase());
+    }
+
+    public void rotor1(MouseEvent mouseEvent) {
+
+        Rotor1.getValue();
+        int r1 = (int) Rotor1.getValue();
+        Rotor1Display.setText(""+ r1);
+
+    }
+
+    public void rotor2(MouseEvent mouseEvent) {
+
+    }
+
+    public void rotor3(MouseEvent mouseEvent) {
+
+    }
+
+    public void generateCipher(javafx.event.ActionEvent actionEvent) {
+
     }
 
 
     //Understand how sliders work
     // - and then assign letters to the textfield
-    // Use Enigma file to complete the architect.
-
-    // Figure out why fxml can't load?
+    // Use Enigma file to complete the architecture.
 
 }
